5b3e78e85bff8c29791ea4390d480daa4dfa6c50
locnguyen9696 <locnguyen7779@gmail.com>
2019-11-07 20:26:41 +0700

    chuc nang them nguoi dung

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,4 +1,8 @@
 var nguoiDungService = new NguoiDungService();
+
+function themTestGit() {
+    console.log("Them nguoi dung");
+};
 getListUser();
 
 getEle("btnThemNguoiDung").addEventListener("click", function () {
